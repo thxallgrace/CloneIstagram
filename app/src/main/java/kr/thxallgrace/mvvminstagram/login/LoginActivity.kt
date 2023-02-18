7be3c054df191069c49acec12b173be5701741d9
thxallgrace <thxall92@gmail.com>
2023-02-18 23:19:12 +0900

    connect firebase login & signin

--- app/src/main/java/kr/thxallgrace/mvvminstagram/login/LoginActivity.kt
+++ app/src/main/java/kr/thxallgrace/mvvminstagram/login/LoginActivity.kt
@@ -3,8 +3,11 @@ package kr.thxallgrace.mvvminstagram.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import kr.thxallgrace.mvvminstagram.R
 import kr.thxallgrace.mvvminstagram.databinding.ActivityLoginBinding
 
@@ -18,6 +21,7 @@ class LoginActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         binding.viewModel = loginViewModel
         binding.activity = this
+        binding.lifecycleOwner = this
 
         setObserve()
     }
@@ -36,11 +40,19 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun loginEmail() {
-        loginViewModel.showFindIdActivity.value = true
-    }
-
     fun findId() {
         loginViewModel.showFindIdActivity.value = true
     }
+
+
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        println("result:${result.toString()}")
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+        println("account:${account}")
+        loginViewModel.firebaseAuthWithGoogle(account.idToken)  // 로그인한 사용자 정보를 암호화한 값
+    }
+
 }
--- app/src/main/java/kr/thxallgrace/mvvminstagram/login/LoginViewModel.kt
+++ app/src/main/java/kr/thxallgrace/mvvminstagram/login/LoginViewModel.kt
@@ -1,12 +1,63 @@
 package kr.thxallgrace.mvvminstagram.login
 
+import android.app.Application
+import android.content.Context
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kr.thxallgrace.mvvminstagram.R
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+    private val auth = FirebaseAuth.getInstance()
     var id : MutableLiveData<String> = MutableLiveData("")
     var password : MutableLiveData<String> = MutableLiveData("")
 
     var showInputNumberActivity : MutableLiveData<Boolean> = MutableLiveData(false)
     var showFindIdActivity : MutableLiveData<Boolean> = MutableLiveData(false)
+    val context = getApplication<Application>().applicationContext
+
+    var googleSignInClient : GoogleSignInClient
+
+    init {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(context, gso)
+    }
+    fun loginWithSignupEmail() {
+        println("loginWithSignupEmail")
+        auth.createUserWithEmailAndPassword(id.value.toString(), password.value.toString()).addOnCompleteListener {
+            if(it.isSuccessful){
+                showFindIdActivity.value = true
+            }else{
+                //아이디가 중복인경우?
+            }
+        }
+    }
+
+    fun loginGoogle(view: View) {
+        val i = googleSignInClient.signInIntent
+        (view.context as? LoginActivity)?.googleLoginResult?.launch(i)
+    }
+    fun firebaseAuthWithGoogle(idToken: String?) {
+        println("idToken : $idToken")
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            println("isSuccess? ${it.isSuccessful}")
+            if(it.isSuccessful){
+                showFindIdActivity.value = true
+            }else{
+                //아이디가 중복인경우?
+            }
+        }
+    }
+
 }
